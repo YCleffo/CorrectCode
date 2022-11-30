4c9142ceecb0e36d082b425da0fe9ab1f212698e
emk7online <emk7online@gmail.com>
2022-11-30 13:12:39 +0500

    Добавьте файлы проекта.

---
+++ CorrectCodeClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrectCodeLibrary
+{
+    public class CorrectCodeClass
+    {
+        /// <summary>
+        /// Событие нажатия на кнопку будет создавать объект класса и вызывать метод
+        /// </summary>
+        /// <param name="password">
+        /// Метод выполняет  проверку правильности  заполнения текстового поля</param>
+        /// <returns>
+        /// Метод возвращает true
+        /// </returns>
+        public static bool IsCorrectCode(string candidate)
+        {
+            char[] codecanArray = candidate.ToCharArray();
+            if(candidate.Length<10|!candidate.All(char.IsDigit)| candidate.Length > 10)
+            {
+                return false;
+            }
+            else
+            {
+                int sum = 0;
+                for(int i=0; i< codecanArray.Length; i++)
+                {
+                    sum += codecanArray[i];
+                }
+
+                if (sum % 10 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
